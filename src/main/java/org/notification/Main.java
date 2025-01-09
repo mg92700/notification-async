53025c7543be61d06a665718319d852dea93b116
mg9700 <ghoraf.mouhsin@gmail.com>
2025-01-09 21:03:37 +0100

    add

--- src/main/java/org/notification/Main.java
+++ src/main/java/org/notification/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationAsync {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationAsync.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
